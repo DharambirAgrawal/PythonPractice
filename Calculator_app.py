0e064a5a83ecf66960df326ea948151429e13c24
Dharambirbro <dev.dharambir@gmail.com>
2024-10-06 11:58:27 -0500

    Added styles and font

--- Calculator_app.py
+++ Calculator_app.py
@@ -1,89 +1,99 @@
 #import Modules
-from unittest import result
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QPushButton,QVBoxLayout,QHBoxLayout,QLineEdit,QGridLayout
-from numpy import delete
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QVBoxLayout,QHBoxLayout,QLineEdit,QGridLayout
+from PyQt5.QtGui import QFont
 
-# Main apps objects and settings
-app =QApplication([])
-main_window=QWidget()
-main_window.setWindowTitle("Calculator App")
-main_window.resize(250,300)
+class CalculatorApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        # Main apps objects and settings
+        # main_window=QWidget()  === self
 
-# Create all apps Objects/widgets
+        self.setWindowTitle("Calculator App")
+        self.resize(250,300)
 
-text_box=QLineEdit()
-grid =QGridLayout()
+        # Create all apps Objects/widgets
 
-buttons=[
-         '7','8','9','/',
-         '4','5','6','*',
-         '1','2','3','-',
-         '0','.','=','+'
-         ]
-clear=QPushButton("C")
-delete=QPushButton("DEL")
+        self.text_box=QLineEdit()
+        # self.text_box.setFont(QFont('Arial',20)) #set the font size of the text box
+        self.text_box.setFont(QFont('Helvetica',20)) #set the font size of the text box
 
-def button_clicked():
-    button=app.sender()
-    text=button.text() #get the text of the button
-    print(text)
+        self.grid =QGridLayout()
 
-    if text == "=":
-        symbol=text_box.text() #get the text in the text box
-        try:
-            result=eval(symbol)
-            text_box.setText(str(result))
-        except Exception as e:
-            print(e)
-            text_box.setText("Error")
-
-    elif text == "C":
-        text_box.clear()
-    elif text == "DEL":
-        text=text_box.text() #get the text in the text box
-        text=text[:-1] #remove the last character
-        text_box.setText(text)
-    else:
-        current_value=text_box.text() #get the text in the text box
-        text_box.setText(current_value+text) #append the text of the button to the text box
-
-row=0
-col=0
-for text in buttons:
-    button=QPushButton(text)
-    button.clicked.connect(button_clicked)
-    grid.addWidget(button,row,col)
-    col+=1
-    if col>3:
+        self.buttons=[
+                '7','8','9','/',
+                '4','5','6','*',
+                '1','2','3','-',
+                '0','.','=','+'
+                ]
+        
+        row=0
         col=0
-        row+=1
+        for text in self.buttons:
+            button=QPushButton(text)
+            button.clicked.connect(self.button_clicked)
+            self.setStyleSheet("QPushButton {font: 20px Comic Sans MS; padding: 10px;}") #set the font size of the buttons using CSS
+            self.grid.addWidget(button,row,col)
+            col+=1
+            if col>3:
+                col=0
+                row+=1
+
+        self.clear=QPushButton("C")
+        self.delete=QPushButton("DEL")
+        self.delete.setStyleSheet("QPushButton {background-color:  #f0f0f8; }") #set the background color of the button
+        self.clear.setStyleSheet("QPushButton {background-color: #ff0000; color: #ffffff;}") #set the background color of the button
+
+        #designs
+        master_layout=QVBoxLayout()
+
+        master_layout.addWidget(self.text_box)
+        master_layout.addLayout(self.grid)
 
 
+        button_row=QHBoxLayout()
+        button_row.addWidget(self.clear)
+        button_row.addWidget(self.delete)
+        master_layout.addLayout(button_row)
+
+        # master_layout.setContentsMargins(10,10,10,10) #set the margin of the layout
+
+        self.setLayout(master_layout)
+
+        #events
+        self.clear.clicked.connect(self.button_clicked)
+        self.delete.clicked.connect(self.button_clicked)
 
 
+    def button_clicked(self):
+        button=app.sender()
+        text=button.text() #get the text of the button
 
+        if text == "=":
+            symbol=self.text_box.text() #get the text in the text box
+            try:
+                result=eval(symbol)
+                self.text_box.setText(str(result))
+            except Exception as e:
+                print(e)
+                self.text_box.setText("Error")
 
-#All designes here
-master_layout=QVBoxLayout()
-
-master_layout.addWidget(text_box)
-master_layout.addLayout(grid)
-
-button_row=QHBoxLayout()
-button_row.addWidget(clear)
-button_row.addWidget(delete)
-
-
-master_layout.addLayout(button_row)
-
-main_window.setLayout(master_layout)
-
-#events
-clear.clicked.connect(button_clicked)
-delete.clicked.connect(button_clicked)
+        elif text == "C":
+            self.text_box.clear()
+        elif text == "DEL":
+            text=self.text_box.text() #get the text in the text box
+            text=text[:-1] #remove the last character
+            self.text_box.setText(text)
+        else:
+            current_value=self.text_box.text() #get the text in the text box
+            self.text_box.setText(current_value+text) #append the text of the button to the text box
 
 
 # show/run our app
-main_window.show()
-app.exec_()
+
+if __name__=="__main__":
+        
+    app =QApplication([])
+    main_window=CalculatorApp()
+    main_window.setStyleSheet("QWidget {background-color: #f0f0f8}") #set the background color of the main window
+    main_window.show()
+    app.exec_()

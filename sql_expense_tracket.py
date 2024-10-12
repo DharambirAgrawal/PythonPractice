2797205a414fc89a3a925204231cc1d5efdfb50a
Dharambirbro <dev.dharambir@gmail.com>
2024-10-12 16:43:34 -0500

    Completed SQL EXPENSE TRACKER PROJECT

--- sql_expense_tracket.py
+++ sql_expense_tracket.py
@@ -1,5 +1,8 @@
 #import modules
-from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QVBoxLayout,QHBoxLayout,QComboBox,QLabel,QDateEdit,QLineEdit,QTableWidget
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QVBoxLayout,QHBoxLayout,QComboBox,QLabel,QDateEdit,QLineEdit,QTableWidget,QMessageBox,QTableWidgetItem
+from PyQt5.QtCore import QDate
+from PyQt5.QtSql import QSqlDatabase,QSqlQuery
+import sys
 
 
 #App class
@@ -23,6 +26,12 @@ class ExpenseTracker(QWidget):
         self.table=QTableWidget()
         self.table.setColumnCount(5)  #Id, Date, Category, Description, Amount
         self.table.setHorizontalHeaderLabels(["Id","Date","Category","Description","Amount"])
+
+        #adding drop downs
+        self.dropdown.addItems(["Food","Transport","Rent","Entertainment","Bills","Other"])
+
+        #setting date
+        self.date_box.setDate(QDate.currentDate())
 
         #app designs
         self.master_layout=QVBoxLayout()
@@ -55,8 +64,107 @@ class ExpenseTracker(QWidget):
 
         #setting the master layout
         self.setLayout(self.master_layout)
+        self.load_table()
 
         #events
+        self.add_btn.clicked.connect(self.add_expense)
+        self.delete_btn.clicked.connect(self.delete_expense)
+
+    
+    def load_table(self):
+        self.table.setRowCount(0)
+
+        query=QSqlQuery("SELECT * FROM expenses")
+        row =0
+
+        while query.next():
+            expense_id=query.value(0)
+            date=query.value(1)
+            category=query.value(2)
+            description=query.value(3)
+            amount=query.value(4)
+
+            # add the data to the table
+
+            self.table.insertRow(row)
+            self.table.setItem(row,0,QTableWidgetItem(str(expense_id))) #row, column, value
+            self.table.setItem(row,1,QTableWidgetItem(date))
+            self.table.setItem(row,2,QTableWidgetItem(str(category)))
+            self.table.setItem(row,3,QTableWidgetItem(str(description)))
+            self.table.setItem(row,4,QTableWidgetItem(str(amount)))
+            row+=1
+
+    def add_expense(self):
+        date=self.date_box.date().toString("yyyy-MM-dd")
+        category=self.dropdown.currentText()
+        description=self.description.text()
+        amount=self.amount.text()
+
+        query=QSqlQuery()
+        query.prepare("""
+                      INSERT INTO expenses (date,category,description,amount) 
+                      VALUES (:date,:category,:description,:amount)
+                      """)
+        query.bindValue(":date",date)
+        query.bindValue(":category",category)
+        query.bindValue(":description",description)
+        query.bindValue(":amount",amount)
+        query.exec_()
+
+        self.date_box.setDate(QDate.currentDate())
+        self.dropdown.setCurrentIndex(0)
+        self.description.clear()
+        self.amount.clear() 
+
+        self.load_table()
+
+    def delete_expense(self):
+        row=self.table.currentRow() # gives row starting from 0 so we have to add +1
+        if row<0:
+            QMessageBox.warning(None,"Error","Select a row to delete")
+            return
+        
+        expense_id=int(self.table.item(row,0).text())
+        
+        confirm=QMessageBox.question(self,"Confirm","Are you sure you want to delete this expense?",QMessageBox.Yes|QMessageBox.No) #returns a number
+        if confirm==QMessageBox.No:
+            return
+        
+        query=QSqlQuery()
+        query.prepare("DELETE FROM expenses WHERE id=:id")
+        query.bindValue(":id",expense_id)
+        query.exec_()
+        self.load_table()
+
+        
+
+
+
+
+
+# create a database connection
+
+database=QSqlDatabase.addDatabase("QSQLITE")
+database.setDatabaseName("expenses.db")
+if not database.open():
+    QMessageBox.critical(None,"Error","Error in opening database")
+    print("Error in opening database")
+    sys.exit(1)
+
+#create a table
+
+query=QSqlQuery()
+query.exec_("""
+            CREATE TABLE IF NOT EXISTS expenses (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date TEXT,
+                category TEXT,
+                description TEXT,
+                amount REAL
+            )
+            """)
+
+
 
 
 #run the app
